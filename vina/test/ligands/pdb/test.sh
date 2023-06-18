b3a0d5361915a24db00a7794167587d08d77f8d2
mirjr <mirsultonov_m@nuu.uz>
2023-06-18 16:34:25 +0500

    edited

--- vina/test/ligands/pdb/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for l in *.pdb; do
-        b=`basename $l .pdb`
-        mkdir -p ../pdbqt
-        prepare_ligand -l $l -o ../pdbqt/${b}.pdbqt
-done
-
--- vina/test/ligands/pdb_convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for l in ./sdf/*.sdf; do
-        b=`basename $l .sdf`
-        mkdir -p ./pdb
-        obabel -isdf $l -opdb -O ./pdb/${b}.pdb -m
-done
