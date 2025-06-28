b7bf772171be7ad92f37ece4bca4f6cbccdd8ea6
mirrrrrrrrrrrr <mirrrrrr@yahoo.com>
2025-06-29 01:04:56 +0500

    7bv2

--- 02-autodock-vina/16-work/7bv2/run.sh
+++ 02-autodock-vina/16-work/7bv2/run.sh
@@ -4,6 +4,6 @@ for f in ./ligands/*.pdbqt; do
 	b=`basename $f .pdbqt`
 	echo Proccessing ligand $b
 	mkdir -p ./dist/$b
-	vina --config conf.txt --ligand $f --out ./dist/${b}/out.pdbqt
+	vina --cpu 3 --config conf.txt --ligand $f --out ./dist/${b}/out.pdbqt
 done
 
